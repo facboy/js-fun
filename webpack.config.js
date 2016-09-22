0b8e53891763412c14eca2d0f4e6e42541fa3ea7
Christopher Ng <chris.ng@byhiras.com>
2016-09-22 09:57:10 +0000

    Fixed webpack-dev-server config.

--- webpack.config.js
+++ webpack.config.js
@@ -1,14 +1,19 @@
-var path = require('path');
+var os = require('os'),
+    path = require('path');
 
 module.exports = {
     entry: "./src/entry.js",
     output: {
         path: path.join(__dirname, 'build'),
+        publicPath: '/build/',
         filename: "bundle.js"
     },
     module: {
         loaders: [
-            { test: /\.css$/, loader: "style!css" }
+            { test: /\.css$/, loader: "style!css" },
         ]
+    },
+    devServer: {
+        host: os.hostname()
     }
 };
